0823226d8f043d3b71de2801174ec481de30a875
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-02-04 20:15:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function audiot1(t1) {
+  t1.classList.toggle("C4");
+  const som = new Audio("sons/xylophone-c4.wav");
+  som.volume = 1;
+  som.play();
+}
+function audiot2(t2) {
+  t2.classList.toggle("RD");
+}
+function audiot3(t3) {
+  t3.classList.toggle("ME");
+}
+function audiot4(t4) {
+  t4.classList.toggle("FF");
+}
+function audiot5(t5) {
+  t5.classList.toggle("SG");
+}
+function audiot6(t6) {
+  t6.classList.toggle("LA");
+}
+function audiot7(t7) {
+  t7.classList.toggle("SB");
+}
+function audiot8(t8) {
+  t8.classList.toggle("DCc");
+}
